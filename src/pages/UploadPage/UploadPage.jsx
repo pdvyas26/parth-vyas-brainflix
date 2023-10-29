4b8c8c4c8aa30d7e4a3a044483741244e8289a3f
pdvyas26 <pdvyas26@gmail.com>
2023-10-29 09:58:47 -0700

    add desktop view on upload page

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -6,11 +6,26 @@ import PublishIcon from "../../assets/images/Icons/publish.svg"
 function UploadPage (){
   return (
     <div className="upload">
+
         <h1 className="upload__title">Upload Video</h1>
-        <p className="upload__heading">VIDEO THUMBNAIL</p>
-      <div className="upload__image-container">
-        <img className="upload__image" src={UploadImage} alt="Upload Image" />
-      </div>
+
+        <div className="upload__desktop-view">
+          <div className="upload__desktop-view-left">
+            <p className="upload__heading">VIDEO THUMBNAIL</p>
+            <div className="upload__image-container">
+              <img className="upload__image" src={UploadImage} alt="Upload Image" />
+            </div>
+          </div>
+
+          <div className="upload__desktop-view-right">
+            <p className="form__title">TITLE YOUR VIDEO</p>
+            <input className="form__input" type="text" placeholder="Add a title to your video"/>
+            <p className="form__description-title">ADD A VIDEO DESCRIPTION</p>
+            <textarea className="form__description" placeholder="Add a description to your video" cols="30" rows="6"></textarea>
+          </div>
+        </div>
+        
+
       <div className="form">
         <div className="form__container">
           <p className="form__title">TITLE YOUR VIDEO</p>
